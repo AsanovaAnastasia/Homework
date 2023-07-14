f1b05f9e2a48a9165242e41c63f309b4852a7ac3
Anastasia <bisiurko.anastasia@gmail.com>
2023-07-14 22:46:51 +0300

    Задача 27

--- Program.cs
+++ Program.cs
@@ -10,3 +10,25 @@ int numB = Convert.ToInt32(Console.ReadLine());
 double num = Math.Pow (numA,numB);
 
 Console.WriteLine(num);
+
+
+
+// // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// // 452 -> 11
+// // 82 -> 10
+// // 9012 -> 12
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int temp = 0;
+
+while (num > 0)
+{
+    temp = num % 10;
+    num = num / 10;
+    sum += temp;
+   
+}
+Console.WriteLine($"Сумма цифр введенного числа равна {sum}");
