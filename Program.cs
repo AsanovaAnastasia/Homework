26b13093092b2b22b33b864ca1b2adf28392f4d3
Anastasia <bisiurko.anastasia@gmail.com>
2023-07-14 23:33:46 +0300

    Задача 29

--- Program.cs
+++ Program.cs
@@ -32,3 +32,19 @@ while (num > 0)
    
 }
 Console.WriteLine($"Сумма цифр введенного числа равна {sum}");
+
+
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] nums = new int[8];
+int n = nums.Length;
+
+for (int i = 0; i < n; i++)
+{
+    nums[i] = new Random().Next();
+    Console.Write($"{nums[i]} ");
+}
