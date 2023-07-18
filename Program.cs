395f2d0825a796b669438c1987c00d3aef853653
Anastasia <bisiurko.anastasia@gmail.com>
2023-07-18 12:35:05 +0300

    Задача 36

--- Program.cs
+++ Program.cs
@@ -43,3 +43,48 @@ Fill(array);
 Print(array);
 Console.WriteLine();
 sum();
+
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+
+void Fill(int[] numbers)
+{
+    int n = numbers.Length;
+    int i = 0;
+    while (i < n)
+    {
+        numbers[i] = new Random().Next(50);
+        i++;
+    }
+}
+void Print(int[] nums)
+{
+    int n = nums.Length;
+    int i = 0;
+    while (i < n)
+    {
+        Console.Write(nums[i] + " ");
+        i++;
+    }
+}
+
+void sum(){
+    int n = array.Length;
+    int i = 0;
+    int summa = 0;
+    while (i<n){
+        summa += array[i];
+        i= i+2;
+    }
+    Console.WriteLine($"Сумма чисел стоящих на нечетных позициях : {summa}");
+}
+
+Fill(array);
+Print(array);
+Console.WriteLine();
+sum();
+
