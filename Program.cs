2d85c55174b5252330e06b12b03933e6295fc3ee
Anastasia <bisiurko.anastasia@gmail.com>
2023-07-18 13:31:04 +0300

    Задача 36

--- Program.cs
+++ Program.cs
@@ -26,12 +26,15 @@ void Print(int[] nums)
     }
 }
 
-void sum(){
+void sum()
+{
     int n = array.Length;
     int i = 0;
     int summa = 0;
-    while (i<n){
-        if (array[i] % 2 == 0){
+    while (i < n)
+    {
+        if (array[i] % 2 == 0)
+        {
             summa += 1;
         }
         i++;
@@ -72,13 +75,15 @@ void Print(int[] nums)
     }
 }
 
-void sum(){
+void sum()
+{
     int n = array.Length;
     int i = 0;
     int summa = 0;
-    while (i<n){
+    while (i < n)
+    {
         summa += array[i];
-        i= i+2;
+        i = i + 2;
     }
     Console.WriteLine($"Сумма чисел стоящих на нечетных позициях : {summa}");
 }
@@ -88,3 +93,58 @@ Print(array);
 Console.WriteLine();
 sum();
 
+
+
+
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] array = new double[10];
+
+void Fill(double[] numbers)
+{
+    int n = numbers.Length;
+    int i = 0;
+    while (i < n)
+    {
+        numbers[i] = Math.Round(new Random().NextDouble() * 100, 2);
+        i++;
+    }
+
+}
+void Print(double[] nums)
+{
+    int n = nums.Length;
+    int i = 0;
+    while (i < n)
+    {
+        Console.Write(nums[i] + "   ");
+        i++;
+    }
+}
+
+void MinMax()
+{
+    int i = 0;
+    int n = array.Length;
+    double min = array[0];
+    double max = array[0];
+
+    while (i<n){
+        if (array[i]> max){
+            max = array[i];
+        }
+        else if (array[i]< min){
+            min = array[i];
+        }
+        i++;
+    }
+    Console.WriteLine($"Минимальное вещественное число массива: {min}");
+    Console.WriteLine($"Максимальное вещественное число массива: {max}");
+}
+
+
+Fill(array);
+Print(array);
+Console.WriteLine();
+MinMax();
