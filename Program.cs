b72518edeee1dbd63af532628e3648c6e0eaac1e
Anastasia <bisiurko.anastasia@gmail.com>
2023-08-03 20:28:23 +0300

    Задача 66

--- Program.cs
+++ Program.cs
@@ -25,3 +25,22 @@ string Numbers(int n, int a)
     else return string.Empty;
 }
 Console.WriteLine(Numbers(n, a));
+
+
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int m, int n)
+{
+    int sum = 0;
+    if (m <= n) return sum += m + Sum(m + 1, n);
+    else return sum;
+}
+Console.WriteLine(Sum(m, n));
